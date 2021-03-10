3714b171b50964755417f4cb66051ea2f4bf6611
Marvin <dawid.bednarczyk@protonmail.com>
2021-03-10 11:17:32 +0100

    cmp2

--- test/stringCmp.cc
+++ test/stringCmp.cc
@@ -10,12 +10,17 @@ int main(){
     b="string2";
     c="string3";
 
+    string zdanie = ": jest wiekszy";
+    
     (a == b) ? (cout << true) : (cout << false);
     cout << endl;
     (a == a) ? (cout << true) : (cout << false);
     cout << endl;
     (a != b) ? (cout << true) : (cout << false);
 
-    cout << "napis a " <<a<< " imie";
+    cout << "napis a " <<a<< " imie" << endl;
+
+    (a < b) ? (cout << "b " << b <<zdanie << endl) : (cout <<"a" << a << zdanie << endl);
+
     return 0;
 }
