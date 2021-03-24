eabb76854595c80cdd5a977f549680490a710f1d
Marvin <dawid.bednarczyk@protonmail.com>
2021-03-24 11:36:44 +0100

    perform

--- Zad3.3/ZadArytmetykaWskaznikow.cc
+++ Zad3.3/ZadArytmetykaWskaznikow.cc
@@ -1,11 +1,20 @@
 #include<iostream>
 
 using namespace std;
+void operations();
+void perform();
 
 int main(){
+    operations();
+    perform();
+    return 0;
+}
+
+void operations(){
     int *p;
     int zmienna = 3;
     p = &zmienna;
+    cout << p;
     cout << endl << p++;
     cout << endl << ++p;
     //cout << ++*p;
@@ -16,5 +25,35 @@ int main(){
     cout << endl <<*(++p) << endl;
 
     cout << endl << endl << "wartosc *p " <<*p << ", zmienna " << zmienna;
-    return 0;
+}
+
+void perform(){
+    int a=0,b=0,c=0;
+    int zmienna = 10;
+    int *wsk;
+    a = zmienna;
+    cout << endl <<"adresy: a,b,c: " << &a << " " << &b << " " << &c << endl;
+    cout << "a " << a << "\t" << "b " << b << "\t" << "c " << c << endl;
+    b = 10;
+    cout << "a " << a << "\t" << "b " << b << "\t" << "c " << c << endl;
+    wsk = &c;
+    *wsk = 20;
+    cout << "a " << a << "\t" << "b " << b << "\t" << "c " << c << endl;
+    *(&a + 1) = 30;
+    cout << "a " << a << "\t" << "b " << b << "\t" << "c " << c << endl;
+    *(&c - 2) = 40;
+    cout << "a " << a << "\t" << "b " << b << "\t" << "c " << c << endl;
+    *( (int*)&a + 1) = 50;
+    cout << "a " << a << "\t" << "b " << b << "\t" << "c " << c << endl;
+    *(int *)(&a + 2) = 60;
+    cout << "a " << a << "\t" << "b " << b << "\t" << "c " << c << endl;
+    //*(int*)(&wsk - 1) = 70;
+    //cout << "a " << a << "\t" << "b " << b << "\t" << "c " << c << endl;
+    wsk = &b;
+    *wsk = 70;
+    cout << "a " << a << "\t" << "b " << b << "\t" << "c " << c << endl;
+    *(&b - 1) = 80;
+    cout << "a " << a << "\t" << "b " << b << "\t" << "c " << c << endl;
+    *wsk = 90;
+    cout << "a " << a << "\t" << "b " << b << "\t" << "c " << c << endl;
 }
