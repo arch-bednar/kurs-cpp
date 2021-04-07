8b2f1bfc86485a0faebdc793cf0a57b327479720
Marvin <dawid.bednarczyk@protonmail.com>
2021-04-07 10:14:58 +0200

    unia

--- Zad4.4/ZadUnia.cc
+++ Zad4.4/ZadUnia.cc
@@ -1,8 +1,16 @@
 #include<iostream>
 
+using namespace std;
+
 int main(){
   int n=255;
   n = n&128;//& bitowa unia, działa na bitach porównując dwie liczby, && unia logiczna (do warunków)
-  std::cout << n;
+  std::cout << n << endl;
+  int w = -1;
+  cout << (w&13) << endl; //w przypadku liczby dodatniej i ujemnej wyjdzie nam liczba dodatnia
+  //w cout musimy operację bitową w&13 podać w nawiasie, inaczej program sie wysypie
+
+  char a = 'a';
+  cout <<(int)a<< ' ' << (a&97)<<endl;
   return 0;
 }
