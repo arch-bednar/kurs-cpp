760e867c14b87a07c412ead1515f29909180d864
Marvin <dawid.bednarczyk@protonmail.com>
2021-05-26 11:43:33 +0200

    zad5.6

---
+++ Zad5.6/Rzad.hpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+
+class Rzad{
+
+public:
+  Rzad(){}
+
+  void ileMetrow(){
+    std::cout << sqrt(4/3.14);
+  }
+};
---
+++ Zad5.6/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include"Kolo.hpp"
+#include"Kwadrat.hpp"
+#include"Rzad.hpp"
+using namespace std;
+
+int main(){
+  Kolo k(4);
+  k.toString();
+  cout << k.Pole() << endl;
+  cout << k.Obwod() << endl;
+
+  std::cout << std::endl;
+
+  Kwadrat kw(3,4);
+  kw.toString();
+  cout << kw.Pole() << endl;
+  cout << kw.Obwod() << endl;
+
+  Rzad r;
+  cout << "Obywatele mają powierzchnię okręgu o promieniu: ";
+  r.ileMetrow();
+}
